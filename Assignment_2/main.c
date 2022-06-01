4d7854caedb3ef4519f14c6798696a230473fba0
Dohyeok_Kwon <kwon1585@gmail.com>
2022-06-01 15:27:21 +0900

    .

--- Assignment_2/main.c
+++ Assignment_2/main.c
@@ -3,16 +3,14 @@
 int main(void)
 {
   FILE *file_in = fopen("input.txt", "rt");
-  //FILE *file_out = fopen("output.txt", "wt");
+  FILE *file_out = fopen("output.txt", "wt");
 
   char maze[MAX][MAX] = {0,};
   create_maze(file_in, maze);
-  for (int i = 0; i < MAX; i++)
-  {
-    printf("%c ", maze[0][i]);
-  }
+  
 
-  print_maze();
+  print_maze(file_out, maze);
 
   return 0;
 }
+
--- Assignment_2/maze.c
+++ Assignment_2/maze.c
@@ -2,12 +2,24 @@
 
 void create_maze(FILE *file, char (*maze)[MAX])
 {
-  printf("%p\n", file);
-  maze[0][0] = 'H';
-  printf("hi\n");
+  int row = 0;
+  char buf[MAX] = {0, };
+
+  while (fgets(buf, MAX, file))
+  {
+    for (int col = 0; buf[col] != '\n'; col++)
+      maze[row][col] = buf[col];
+    row++;
+  }
 }
 
-void print_maze()
+void print_maze(FILE *file, char (*maze)[MAX])
 {
-
+  file = 0;
+  for (int i = 0; i<10; i++)
+  {
+    for (int j = 0; j<10; j++)
+      printf("%c", maze[i][j]);
+    printf("\n");
+  }
 }
